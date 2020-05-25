a0c1b5d5544bfd5b4e41565d1f7ea719467ecc4f
waking-eni <61460471+waking-eni@users.noreply.github.com>
2020-05-25 17:43:33 +0200

    Update sidebar.php

--- public/sidebar.php
+++ public/sidebar.php
@@ -1,7 +1,35 @@
-<div class="col sidebar bg-dark mt-0 pb-0">
-    <h3 class="white-font">Category</h3>
+<?php
+   include_once __DIR__.'/../database/product.php';
+?>
+
+<div class="col sidebar bg-light mt-0 pb-0">
+    <h3>Categories</h3>
 
     <ul class="list-unstyled">
+
+        <?php
+        
+        try {
+            $product = new Product();
+            $categories = $product->getAllCategories();
+        } catch(Exception $e) {
+            echo 'Caught exception: '.$e->getMessage();
+        }
+
+        //list all categories
+        if($categories && !empty($categories)) {
+            foreach($categories as $key => $category) {
+
+                echo '<li class="nav-item">';
+                    echo '<a class="nav-link active red-font" href="listByCategory.php?category='
+                    .$category["name"].'">'.$category['name'].'</a>';
+                echo '</li>';
+
+            }
+        }
+
+        ?>
+
     </ul>
 
 </div>
