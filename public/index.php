269a3e0534fd099da27d0de1a7ae24dc71d1e2c7
waking-eni <61460471+waking-eni@users.noreply.github.com>
2020-05-25 12:42:17 +0200

    Create index.php

---
+++ public/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--bootstrap-->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" 
+    integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" 
+    crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" 
+    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" 
+    crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" 
+    integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" 
+    crossorigin="anonymous"></script>
+    <title>Buy</title>
+    <link rel="stylesheet" href="../design/style.css">
+</head>
+
+<body>
+
+<!--Navigation bar-->
+<?php
+    include 'header.php';
+?>
+<!--end of navigation-->
+
+<!--wrapper-->
+<div class="container-fluid wrapper-container">
+    <div class="row row-wrapper">
+
+    <!--sidebar-->
+    <?php
+        include 'sidebar.php';
+    ?>
+    <!--end of sidebar-->
+
+    </div>
+</div>
+<!--end of wrapper-->
+
+</body>
+
+</html>
