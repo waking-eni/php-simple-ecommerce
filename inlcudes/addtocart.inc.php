53e0f6b2673bb9bde44a461c4457c60e852aa0e1
waking-eni <61460471+waking-eni@users.noreply.github.com>
2020-05-26 18:58:46 +0200

    Create addtocart.inc.php

---
+++ inlcudes/addtocart.inc.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+Script for the functionality of adding a product to the cart
+*/
+
+if (session_status() == PHP_SESSION_NONE) {
+	session_start();
+}
+
+if(isset($_POST['addtocart'])) {
+    require_once __DIR__.'/../database/dbhandler.php';
+
+    try {
+        $db = Database::getInstance();
+    } catch(Exception $e) {
+        echo 'Caught exception: '.$e->getMessage();
+    }
+
+    $quantity = str_replace(array(':', '-', '/', '*', '<', '>'), '', $_POST['quantity']);
+    $total = str_replace(array(':', '-', '/', '*', '<', '>'), '', $_POST['total']);
+    $productId = str_replace(array(':', '-', '/', '*', '<', '>'), '', $_POST['productId']);
+
+    if(empty($quantity) || empty($total) || empty($productId)) {
+        header("Location: ../public/index.php?error=noproduct");
+        exit();
+    } else {
+        $_SESSION['cart'] = array();
+        $a = array("productId" => $productId, "quantity" => $quantity, "total" => $total);
+        array_push($_SESSION['cart'], $a);
+
+        header("Location: ../public/cart.php");
+        exit();
+    }
+
+} else {
+    header("Location: ../public/index.php");
+	exit();
+}
